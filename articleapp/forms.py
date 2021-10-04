aab58672184351f74db2d89081464dd77e5534d6
woojoo0425 <shinwoojoo1995@yahoo.co.jp>
2021-10-04 14:44:35 +0900

    Django 42 (MultipleObjectMixin)

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -5,4 +5,4 @@ from articleapp.models import Article
 class ArticleCreateForm(ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'image', 'content']
+        fields = ['title', 'image', 'project', 'content']
