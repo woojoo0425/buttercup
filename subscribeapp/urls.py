3ca713324d9928183f6b31832612ab801ffa16b8
woojoo0425 <shinwoojoo1995@yahoo.co.jp>
2021-10-05 13:04:36 +0900

    Django 44 (Field Lookup)

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from subscribeapp.views import SubscribeView
+from subscribeapp.views import SubscribeView, SubscribeListView
 
 app_name = "subscribeapp"
 
 urlpatterns = [
     path('subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path('list/', SubscribeListView.as_view(), name='list'),
 ]
